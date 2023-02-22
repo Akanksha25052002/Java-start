4a74797419e3513d98e61142b74c0251f0b135da
Akanksha25052002 <akankshashrivastava2022@vitbhopal.ac.in>
2023-02-22 23:55:20 +0530

    Exception

---
+++ Exception.java
@@ -0,0 +1,27 @@
+public class Exception{
+public static void main(String args[]){
+
+try{
+int a=Integer.parseInt(args[0]);
+int b=Integer.parseInt(args[1]);
+int c=a/b;
+System.out.println("a/b="+c);
+}
+catch(ArithmeticException ex){
+System.out.println("a/b=0");
+}
+catch(arrayIndexOutofBoundsException ex){
+System.out.println("Insufficient inputs please enter two integer");
+}
+catch(NumbersFormatException ex){
+System.out.println("Only digits acceptable");
+}
+catch(Exception ex){
+System.out.println(ex.getMessage());
+}
+finally{
+System.out.println("Bye");
+}
+}
+}
+
