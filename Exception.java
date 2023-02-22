5e378a7c475f8a34773f73596ffae39c6677f23e
Akanksha25052002 <akankshashrivastava2022@vitbhopal.ac.in>
2023-02-23 00:16:57 +0530

    Exception case

--- Exception.java
+++ Exception.java
@@ -1,24 +1,31 @@
 public class Exception{
 public static void main(String args[]){
 
+//Exception ex=new Exception();
+
 try{
+
 int a=Integer.parseInt(args[0]);
 int b=Integer.parseInt(args[1]);
 int c=a/b;
 System.out.println("a/b="+c);
+
 }
+
 catch(ArithmeticException ex){
 System.out.println("a/b=0");
 }
-catch(arrayIndexOutofBoundsException ex){
+catch(ArrayIndexOutOfBoundsException ex){
 System.out.println("Insufficient inputs please enter two integer");
 }
-catch(NumbersFormatException ex){
+catch(NumberFormatException ex){
 System.out.println("Only digits acceptable");
 }
-catch(Exception ex){
+
+/*catch(Exception ex){
 System.out.println(ex.getMessage());
-}
+}*/
+
 finally{
 System.out.println("Bye");
 }
