19c7dfacb49b2bc6e116ae1ae651645110c1fd4d
Akanksha25052002 <114761053+Akanksha25052002@users.noreply.github.com>
2023-04-09 23:52:54 +0530

    Update Fabonacci.java

--- Fabonacci.java
+++ Fabonacci.java
@@ -1,3 +1,7 @@
+/* In this we are printing the fabonacci series.
+In fibonacci series, next number is the sum of previous two numbers for example 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55 etc. 
+The first two numbers of fibonacci series are 0 and 1.  */
+
 public class Fabonacci{
 public static void main(String args[]){
 int a=0,b=1;
